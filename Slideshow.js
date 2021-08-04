8730156fbdc0fbf66b32802729a1b593daa876ce
Tyger Yang <tbyang@ucsd.edu>
2021-08-04 09:28:37 -0700

    Add width parameter to Slideshow object

--- Slideshow.js
+++ Slideshow.js
@@ -171,7 +171,7 @@ export default class Slideshow extends Component {
   }
 
   render() {
-    const width = this.state.width;
+    const width = this.props.width || this.state.width;
     const height = this.props.height || this.state.height;
     const position = this._getPosition();
     return (
